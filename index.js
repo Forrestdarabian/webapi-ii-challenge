16df411462004632813c91fd6dd7ccef39937901
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-03 15:08:17 -0700

    test1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const postsRouter = require("./posts/postsRouter");
 const server = express();
+const port = process.env.PORT || 1000;
 server.use(express.json());
 server.use("/api/posts", postsRouter);
-server.listen(1000, () => console.log("server is up and running :)"));
+server.listen(port, () => console.log("server is up and running :)"));

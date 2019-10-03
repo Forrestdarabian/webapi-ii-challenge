2ee2090b9d2b9e66ff3d16400dd8446e551c3527
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-03 15:20:11 -0700

    test2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const postsRouter = require("./posts/postsRouter");
 const server = express();
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "welcome to my app!" });
+});
 const port = process.env.PORT || 1000;
 server.use(express.json());
 server.use("/api/posts", postsRouter);
